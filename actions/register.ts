8b5e38ab3a30fb1eb0bd1e0b384e892c84c0c0c3
Hassan <hassan@gmail.com>
2025-12-13 00:44:11 -0800

    Prisma Removed

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,32 @@
 "use server";
 
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
+import * as z from "zod";
 
-export const register = async (values: any) => {
-  const validatedValues = LoginSchema.safeParse(values);
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedValues = RegisterSchema.safeParse(values);
+
   if (!validatedValues.success) {
     return { error: "Invalid Fields !" };
   }
-  return { success: "Email sent !" };
+
+  const { email, password, name } = validatedValues.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingEmail = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingEmail) {
+    return { error: "Email Already taken !" };
+  }
+
+  await db.user.create({ data: { email, password: hashedPassword, name } });
+
+  return { success: "User created !" };
 };
