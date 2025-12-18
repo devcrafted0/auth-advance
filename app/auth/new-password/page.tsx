83c384172e0b949ef67316a68c9463274447c816
Hassan <hassan@gmail.com>
2025-12-18 02:59:41 -0800

    Suspense Errors Fix

--- app/auth/new-password/page.tsx
+++ app/auth/new-password/page.tsx
@@ -1,7 +1,12 @@
 import { NewPasswordForm } from "@/components/auth/new-password";
+import { Suspense } from "react";
 
 const Page = () => {
-  return <NewPasswordForm />;
+  return (
+    <Suspense fallback={null}>
+      <NewPasswordForm />;
+    </Suspense>
+  );
 };
 
 export default Page;
